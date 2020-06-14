6bd00799e0decccdb68737b6e6b6e77e99178577
sebastianpineda <sebastianpineda95@gmail.com>
2020-06-14 02:20:07 -0500

    fixed batch correct bug

--- R/batch_correct.R
+++ R/batch_correct.R
@@ -1,63 +1,58 @@
 #' Perform batch correction on `ACTIONetExperiment` and `SingleCellExperiment` objects.
-
-reduce.and.batch.correct.sce.fastMNN <- function(sce, batch.attr = NULL, reduced_dim = 50, MNN.k = 20, return_V = FALSE, reduction_name = "MNN", BPPARAM = SerialParam()) {
+#' @export
+reduce.and.batch.correct.ace.fastMNN <- function(ace, batch.attr = NULL, reduced_dim = 50, MNN.k = 20, return_V = FALSE, reduction_name = "MNN", BPPARAM = SerialParam()) {
   require(scran)
   require(batchelor)
-  colData(sce) = droplevels(colData(sce))
-  SummarizedExperiment::assays(sce)[["counts"]] = as(SummarizedExperiment::assays(sce)[["counts"]], 'sparseMatrix')
+  colData(ace) = droplevels(colData(ace))
+  SummarizedExperiment::assays(ace)[["counts"]] = as(SummarizedExperiment::assays(ace)[["counts"]], 'sparseMatrix')
 
-  # if( length(batch.attr) ==  1) {
-  #   IDX = split(1:dim(sce)[2], colData(sce)[[batch.attr]])
-  # } else {
-  #   IDX = split(1:dim(sce)[2], batch.attr)
-  # }
-  IDX = .get_ace_split_IDX(ace, attr)
+  IDX = get_ace_split_IDX(ace, attr)
 
-  sce.list = lapply(IDX, function(idx) computeSumFactors(sce[, idx], BPPARAM = BPPARAM ) )
-  sce.list.norm = do.call(batchelor::multiBatchNorm, list(sce.list, BPPARAM = BPPARAM))
+  ace.list = lapply(IDX, function(idx) computeSumFactors(ace[, idx], BPPARAM = BPPARAM ) )
+  ace.list.norm = do.call(batchelor::multiBatchNorm, list(ace.list, BPPARAM = BPPARAM))
 
   # Sort based on 'complexity'
-  merge_order = order(sapply(sce.list.norm, function(sce) dim(sce)[2]), decreasing = TRUE)
+  merge_order = order(sapply(ace.list.norm, function(ace) dim(ace)[2]), decreasing = TRUE)
 
-  sce.norm = do.call(cbind, sce.list.norm)
-  SummarizedExperiment::assays(sce.norm)[["logcounts"]] = as(SummarizedExperiment::assays(sce.norm)[["logcounts"]], "sparseMatrix")
+  ace.norm = do.call(cbind, ace.list.norm)
+  SummarizedExperiment::assays(ace.norm)[["logcounts"]] = as(SummarizedExperiment::assays(ace.norm)[["logcounts"]], "sparseMatrix")
 
   set.seed(0)
-  mnn.out <- do.call(batchelor::fastMNN, c(sce.list.norm, list(k = 20, d = 50, auto.merge = FALSE, merge.order = merge_order, cos.norm = FALSE, assay.type = "logcounts", BPPARAM = BPPARAM)))
+  mnn.out <- do.call(batchelor::fastMNN, c(ace.list.norm, list(k = 20, d = 50, auto.merge = FALSE, merge.order = merge_order, cos.norm = FALSE, assay.type = "logcounts", BPPARAM = BPPARAM)))
 
   S_r = reducedDims(mnn.out)[["corrected"]]
-  rownames(S_r) = colnames(sce.norm)
+  rownames(S_r) = colnames(ace.norm)
   colnames(S_r) = sapply(1:ncol(S_r), function(i) sprintf("PC%d", i))
 
 
-  SingleCellExperiment::reducedDims(sce.norm)[[reduction_name]] <- S_r
+  SingleCellExperiment::reducedDims(ace.norm)[[reduction_name]] <- S_r
 
   if(return_V){
     V = rowData(mnn.out)[["rotation"]]
     colnames(V) = sapply(1:dim(V)[2], function(i) sprintf("PC%d", i))
-    rowData(sce.norm)[["rotation"]] = V
+    rowData(ace.norm)[["rotation"]] = V
   }
 
-  return(sce.norm)
+  return(ace.norm)
 }
 
 #' (It used Harmony for batch-correction: https://github.com/immunogenomics/harmony)
 #'
-#' @param sce Input sce object
-#' @param norm.method Normalization method to use. See normalize.sce() function (default:"default")
-#' (used only if the sce object is not already normalized)
+#' @param ace Input ace object
+#' @param norm.method Normalization method to use. See normalize.ace() function (default:"default")
+#' (used only if the ace object is not already normalized)
 #' @param batch.vec Vector of batches per sample
 #' @param reduced_dim Dimension of SVD used for reducing kernel matrix
 #' @param max.iter Number of SVD iterations
-#' @param passphrase Passphrase for encrypting column names of the sce object for anonymization
+#' @param passphrase Passphrase for encrypting column names of the ace object for anonymization
 #'
-#' @return Reduced sce object with added ReducedDims(sce)
+#' @return Reduced ace object with added ReducedDims(ace)
 #'
 #' @examples
-#' sce = import.sce.from.10X(input_path)
-#' batch.vec = sce$Batch # Assumes sample annotations are in the input_path with "Batch" attribute being provided
-#' sce = reduce.and.batch.correct.sce.Harmony(sce)
-reduce.and.batch.correct.sce.Harmony <- function(sce, batch.vec = NULL, reduced_dim = 50, max.iter = 5, data.slot = "logcounts", normalization.method = "default", reduction.slot = "ACTION", seed = 0, SVD_algorithm = 0) {
+#' ace = import.ace.from.10X(input_path)
+#' batch.vec = ace$Batch # Assumes sample annotations are in the input_path with "Batch" attribute being provided
+#' ace = reduce.and.batch.correct.ace.Harmony(ace)
+reduce.and.batch.correct.ace.Harmony <- function(ace, batch.vec = NULL, reduced_dim = 50, max.iter = 5, data.slot = "logcounts", normalization.method = "default", reduction.slot = "ACTION", seed = 0, SVD_algorithm = 0) {
 	if( !("harmony" %in% rownames(installed.packages())) ) {
 		message("You need to install harmony (https://github.com/immunogenomics/harmony) first for batch-correction.")
 		return
@@ -70,14 +65,15 @@ reduce.and.batch.correct.sce.Harmony <- function(sce, batch.vec = NULL, reduced_
         stop(err)
     }
 
-    sce = reduce.sce(sce, reduced_dim = reduced_dim, max.iter = max.iter, normalization.method = normalization.method, data.slot = data.slot, reduction.slot = reduction.slot, seed = seed, SVD_algorithm = SVD_algorithm)
-    sce = batch.correct.sce.Harmony(sce, batch.vec)
+    ace = reduce.ace(ace, reduced_dim = reduced_dim, max.iter = max.iter, normalization.method = normalization.method, data.slot = data.slot, reduction.slot = reduction.slot, seed = seed, SVD_algorithm = SVD_algorithm)
+    ace = batch.correct.ace.Harmony(ace, batch.vec, reduction.slot = reduction.slot)
 
-    return(sce)
+    return(ace)
 }
 
-batch.correct.sce.Harmony <- function(sce, batch.vec, reduction.slot = "ACTION") {
+#' @export
+batch.correct.ace.Harmony <- function(ace, batch.vec, reduction.slot = "ACTION") {
     require(harmony)
-    reducedDims(sce)[[reduction.slot]] = harmony::HarmonyMatrix(reducedDims(sce)[[reduction.slot]], batch.vec, do_pca = FALSE)
-    return(sce)
+    reducedDims(ace)[[reduction.slot]] = harmony::HarmonyMatrix(reducedDims(ace)[[reduction.slot]], batch.vec, do_pca = FALSE)
+    return(ace)
 }
--- R/util.R
+++ R/util.R
@@ -1,12 +1,13 @@
-.get_ace_split_IDX <- function(ace, attr = NULL){
+#' @export
+get_ace_split_IDX <- function(ace, attr = NULL){
   if( length(attr) ==  1) {
-    split_vec = colData(sce)[[attr]]
+    split_vec = colData(ace)[[attr]]
   }
   else {
     split_vec = attr
   }
   split_vec = droplevels(factor(split_vec))
-  IDX = split(1:dim(sce)[2], split_vec)
+  IDX = split(1:dim(ace)[2], split_vec)
 
   return(IDX)
 }
