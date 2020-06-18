35cf34ee986e59b9f1c2aa6d3eddaac0331379ae
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2020-06-18 13:50:23 -0400

    Changed coerce functions
    Former-commit-id: e583d9249ccc13d0bb4df9b67ccc651ac977fac7

--- R/coerce_ace.R
+++ R/coerce_ace.R
@@ -1,12 +1,22 @@
-#' Coerces a SingleCellExperiment method as an ACTIONetExperiment (ACE) object
+#' Coerces a RangedSummarizedExperiment method as an ACTIONetExperiment (ACE) object
 #'
-#' @param from SingleCellExperiment object
+#' @param from RangedSummarizedExperiment object
 #'
 #' @exportMethod coerce
-setAs("SingleCellExperiment", "ACTIONetExperiment", function(from) {
+setAs("RangedSummarizedExperiment", "ACTIONetExperiment", function(from) {
     new("ACTIONetExperiment", from, 
     rowNets=S4Vectors::SimpleList(), 
     colNets=S4Vectors::SimpleList(), 
     rowFactors=S4Vectors::SimpleList(), 
     colFactors=S4Vectors::SimpleList())
+})
+
+setAs("ACTIONetExperiment", "SingleCellExperiment", function(from) {	
+    new("SingleCellExperiment", as(from, "RangedSummarizedExperiment"), 
+    reducedDims=colFactors(from))
+})
+
+setAs("SingleCellExperiment", "ACTIONetExperiment", function(from) {	
+    new("ACTIONetExperiment", as(from, "RangedSummarizedExperiment"), 
+    colFactors=reducedDims(from))
 })
