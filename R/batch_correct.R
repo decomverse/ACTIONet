f173fea01a5c687d0cdf52ccbd494b4a7cc60e95
sebastianpineda <sebastianpineda95@gmail.com>
2020-06-22 20:43:38 -0500

    update batch correction

--- R/batch_correct.R
+++ R/batch_correct.R
@@ -1,42 +1,60 @@
 #' Perform batch correction on `ACTIONetExperiment` and `SingleCellExperiment` objects.
 #' @export
-reduce.and.batch.correct.ace.fastMNN <- function(ace, batch_attr, reduced_dim = 50, MNN_k = 20, return_V = FALSE, reduction_slot = "MNN",
-    BPPARAM = SerialParam()) {
+reduce.and.batch.correct.ace.fastMNN <- function(ace, batch_attr, reduced_dim = 50, MNN_k = 20, return_V = FALSE, reduction_slot = "MNN", V_slot = NULL,  BPPARAM = SerialParam()) {
     .check_and_load_package("batchelor")
 
-    ace = .check_if_ace(ace)
+    ace = .check_and_convert_se_like(ace, "ACE")
     SummarizedExperiment::assays(ace)[["counts"]] = as(SummarizedExperiment::assays(ace)[["counts"]], "sparseMatrix")
     m_data = metadata(ace)
     IDX = .get_ace_split_IDX(ace, batch_attr)
 
-    ace.list = lapply(IDX, function(idx) computeSumFactors(ace[, idx], BPPARAM = BPPARAM))
-    ace.list.norm = do.call(batchelor::multiBatchNorm, list(ace.list, BPPARAM = BPPARAM))
+    sce.list = lapply(IDX, function(idx){
+        sce = as(ace[, idx], "SingleCellExperiment")
+        sce = computeSumFactors(sce, BPPARAM = BPPARAM)
+    })
+    sce.list.norm = do.call(batchelor::multiBatchNorm, list(sce.list, BPPARAM = BPPARAM))
     # Sort based on 'complexity'
-    merge_order = order(sapply(ace.list.norm, function(ace) dim(ace)[2]), decreasing = TRUE)
+    merge_order = order(sapply(sce.list.norm, function(sce) dim(sce)[2]), decreasing = TRUE)
 
-    ace.norm = do.call(cbind, ace.list.norm)
-    SummarizedExperiment::assays(ace.norm)[["logcounts"]] = as(SummarizedExperiment::assays(ace.norm)[["logcounts"]], "sparseMatrix")
+    sce.norm = do.call(cbind, sce.list.norm)
+    if(!all(colnames(ace) == colnames(sce.norm)))
+      sce.norm = sce.norm[, match(colnames(ace), colnames(sce.norm)) ]
+
+    assays(ace)[["logcounts"]] <- assays(sce.norm)[["logcounts"]]
+    SummarizedExperiment::assays(ace)[["logcounts"]] = as(SummarizedExperiment::assays(ace)[["logcounts"]], "sparseMatrix")
+    sizeFactors(ace) = sizeFactors(sce.norm)
+
+    rm("sce.norm")
+    invisible(gc())
 
     set.seed(0)
-    mnn.out <- do.call(batchelor::fastMNN, c(ace.list.norm, list(k = MNN_k, d = reduced_dim, auto.merge = FALSE, merge.order = merge_order, cos.norm = FALSE,
+    mnn.out <- do.call(batchelor::fastMNN, c(sce.list.norm, list(k = MNN_k, d = reduced_dim, auto.merge = FALSE, merge.order = merge_order, cos.norm = FALSE,
         assay.type = "logcounts", BPPARAM = BPPARAM)))
 
-    S_r = reducedDims(mnn.out)[["corrected"]]
-    rownames(S_r) = colnames(ace.norm)
-    colnames(S_r) = sapply(1:ncol(S_r), function(i) sprintf("PC%d", i))
+    S_r = Matrix::t(reducedDims(mnn.out)[["corrected"]])
+    if(!all(colnames(ace) == colnames(S_r)))
+        S_r = S_r[, match(colnames(ace), colnames(S_r))]
 
+    #rownames(S_r) = colnames(ace)
+    rownames(S_r) = sapply(1:nrow(S_r), function(i) sprintf("PC%d", i))
 
-    ACTIONet::colMaps(ace.norm)[[reduction_slot]] <- Matrix::t(S_r)
+    ACTIONet::colMaps(ace)[[reduction_slot]] <- S_r
     colMapTypes(ace)[[reduction_slot]] = "reduction"
 
 
     if (return_V) {
         V = rowData(mnn.out)[["rotation"]]
         colnames(V) = sapply(1:dim(V)[2], function(i) sprintf("PC%d", i))
-        rowMaps(ace.norm)[["rotation"]] = V
+        if(is.null(V_slot) | length(V_slot) > 1){
+            V_slot = paste(reduction_slot, "rotation", sep = "_")
+        }
+        rowMaps(ace)[[V_slot]] = V
     }
-    metadata(ace.norm) = m_data
-    return(ace.norm)
+    rm("sce.list.norm", "mnn.out")
+    invisible(gc())
+
+    metadata(ace) = m_data
+    return(ace)
 }
 
 #' (It used Harmony for batch-correction: https://github.com/immunogenomics/harmony)
@@ -62,7 +80,7 @@ reduce.and.batch.correct.ace.Harmony <- function(ace, batch_attr, reduced_dim = 
       stop(err)
     }
 
-    ace <- .check_if_ace(ace)
+    ace = .check_and_convert_se_like(ace, "ACE")
 
     ace = reduce.ace(ace, reduced_dim = reduced_dim, max_iter = max_iter, norm_method = norm_method, data.slot = data.slot,
         reduction_slot = reduction_slot, seed = seed, SVD_algorithm = SVD_algorithm)
@@ -77,7 +95,7 @@ batch.correct.ace.Harmony <- function(ace, batch_attr, reduction_slot = "ACTION"
       err = sprintf("You need to install harmony (https://github.com/immunogenomics/harmony) first for batch-correction.\n")
       stop(err)
     }
-    ace <- .check_if_ace(ace)
+    ace = .check_and_convert_se_like(ace, "ACE")
     batch_attr = .get_ace_split_IDX(ace, batch_attr, return_split_vec = TRUE)
     ACTIONet::colMaps(ace)[[reduction_slot]] = harmony::HarmonyMatrix(ACTIONet::colMaps(ace)[[reduction_slot]], meta_data = batch_attr, do_pca = FALSE)
     return(ace)
