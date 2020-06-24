38e557591a6003f2fb4633815b66a8c7b1fc73c7
sebastianpineda <sebastianpineda95@gmail.com>
2020-06-24 07:13:57 -0500

    bug fix
    Former-commit-id: 8cd96474a0719365c35f5dd6cd830a5aee676f2e

--- R/coerce_ace.R
+++ R/coerce_ace.R
@@ -4,12 +4,12 @@
 #'
 #' @exportMethod coerce
 setAs("SummarizedExperiment", "ACTIONetExperiment", function(from) {
-    ace = .ACTIONetExperiment(from, rowNets = S4Vectors::SimpleList(), colNets = S4Vectors::SimpleList(), 
+    ace = .ACTIONetExperiment(from, rowNets = S4Vectors::SimpleList(), colNets = S4Vectors::SimpleList(),
         rowMaps = S4Vectors::SimpleList(), colMaps = S4Vectors::SimpleList())
-    
+
     # rowData(ace) = DataFrame(as.data.frame(rowData(ace))) colData(ace) =
     # DataFrame(as.data.frame(colData(ace)))
-    
+
     return(ace)
 })
 
@@ -21,16 +21,16 @@ setAs("SummarizedExperiment", "ACTIONetExperiment", function(from) {
 setAs("ACTIONetExperiment", "SingleCellExperiment", function(from) {
     SE = as(from, "SummarizedExperiment")
     sce = as(SE, "SingleCellExperiment")
-    
+
     Xs = colMaps(from)
     Xs = Xs[colMapTypes(from) != "internal"]
-    
+
     transposed_factors = as(lapply(Xs, function(X) Matrix::t(X)), "SimpleList")
     reducedDims(sce) = transposed_factors
-    
+
     # rowData(sce) = DataFrame(as.data.frame(rowData(from))) colData(sce) =
     # DataFrame(as.data.frame(colData(from)))
-    
+
     return(sce)
 })
 
@@ -43,18 +43,17 @@ setAs("SingleCellExperiment", "ACTIONetExperiment", function(from) {
     SE = as(from, "SummarizedExperiment")
     rownames(SE) = rownames(from)
     rowData(SE) = rowData(from)
-    
-    
+
+
     ace = as(SE, "ACTIONetExperiment")
     # ace = as(from, 'ACTIONetExperiment')
-    
-    transposed_factors = as(lapply(reducedDims(from), function(x) SummarizedExperiment(assays = list(X = Matrix::t(x)))), 
+
+    transposed_factors = as(lapply(reducedDims(from), function(x) SummarizedExperiment(assays = list(X = Matrix::t(x)))),
         "SimpleList")
     colMaps(ace) = transposed_factors
-    
+
     rowData(ace) = DataFrame(as.data.frame(rowData(ace)))
     colData(ace) = DataFrame(as.data.frame(colData(ace)))
-    
+
     return(ace)
 })
-
