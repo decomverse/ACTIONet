7991b1a733ec18800da173fa79a6af82a6d87e38
Sebastian Pineda <sebastian@Sebastians-MacBook-Pro.local>
2020-10-07 23:36:22 -0500

    added RCpp helper funcs

--- src/RCpp_util.cpp
+++ src/RCpp_util.cpp
@@ -45,41 +45,63 @@ vec roll_var(vec &X){
 
 
 // [[Rcpp::export]]
-NumericVector fast_row_sums(SEXP &A) {	
+NumericVector fast_row_sums(SEXP &A) {
 	vec sum_vec;
     if (Rf_isS4(A)) {
-		sp_mat X = as<arma::sp_mat>(A);		
+		sp_mat X = as<arma::sp_mat>(A);
 		sum_vec = zeros(X.n_rows);
-		
+
 		sp_mat::const_iterator it     = X.begin();
 		sp_mat::const_iterator it_end = X.end();
 		for(; it != it_end; ++it) {
 			sum_vec[it.row()] += (*it);
 		}
     } else {
-		mat X = as<arma::mat>(A);				
-		sum_vec = sum(X, 1);		
-    }     
+		mat X = as<arma::mat>(A);
+		sum_vec = sum(X, 1);
+    }
 
 	return(arma2vec(sum_vec));
 }
 
 // [[Rcpp::export]]
-NumericVector fast_column_sums(SEXP &A) {	
+NumericVector fast_column_sums(SEXP &A) {
 	vec sum_vec;
     if (Rf_isS4(A)) {
-		sp_mat X = as<arma::sp_mat>(A);		
+		sp_mat X = as<arma::sp_mat>(A);
 		sum_vec = zeros(X.n_rows);
-		
+
 		sp_mat::const_iterator it     = X.begin();
 		sp_mat::const_iterator it_end = X.end();
 		for(; it != it_end; ++it) {
 			sum_vec[it.col()] += (*it);
 		}
     } else {
-		mat X = as<arma::mat>(A);				
+		mat X = as<arma::mat>(A);
 		sum_vec = trans(sum(X, 0));
-    }     
+    }
 
 	return(arma2vec(sum_vec));
+}
+
+// Adapted from https://github.com/GreenleafLab/MPAL-Single-Cell-2019/blob/master/scRNA_02_Cluster_Disease_w_Reference_v1.R
+// [[Rcpp::export]]
+Rcpp::NumericVector computeSparseRowVariances(IntegerVector j, NumericVector val, NumericVector rm, int n) {
+  const int nv = j.size();
+  const int nm = rm.size();
+  Rcpp::NumericVector rv(nm);
+  Rcpp::NumericVector rit(nm);
+  int current;
+  // Calculate RowVars Initial
+  for (int i = 0; i < nv; ++i) {
+    current = j(i) - 1;
+    rv(current) = rv(current) + (val(i) - rm(current)) * (val(i) - rm(current));
+    rit(current) = rit(current) + 1;
+  }
+  // Calculate Remainder Variance
+  for (int i = 0; i < nm; ++i) {
+    rv(i) = rv(i) + (n - rit(i))*rm(i)*rm(i);
+  }
+  rv = rv / (n - 1);
+  return(rv);
 }
