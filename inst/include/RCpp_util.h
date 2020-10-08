b4d3952d0a0288f66a32fa1611b22d02ed569d73
Sebastian Pineda <sebastian@Sebastians-MacBook-Pro.local>
2020-10-08 00:03:44 -0500

    updated headers
    Former-commit-id: 4a7f6b9b6ec25814be4e1fbf1f4b2101f227cecf

--- inst/include/RCpp_util.h
+++ inst/include/RCpp_util.h
@@ -2,7 +2,11 @@
 #define RCPP_UTIL_H
 
 namespace ACTIONet {
-    arma::vec roll_var(arma::vec &X);
+  Rcpp::NumericVector arma2vec(const T& x);
+  arma::vec roll_var(arma::vec &X);
+  Rcpp::NumericVector fast_row_sums(SEXP &A);
+  Rcpp::NumericVector fast_column_sums(SEXP &A);
+  Rcpp::NumericVector computeSparseRowVariances(IntegerVector j, NumericVector val, NumericVector rm, int n);
 }
 
 #endif
