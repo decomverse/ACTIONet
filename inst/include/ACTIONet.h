0f078c7e455b84b435b912f5e8e2136d4e44db57
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2020-11-07 21:10:08 -0500

    Updated unification
    Former-commit-id: a411384ccaa1a068d463b4cba04d1e5588a78f12

--- inst/include/ACTIONet.h
+++ inst/include/ACTIONet.h
@@ -191,7 +191,7 @@ namespace ACTIONet {
 	// Post-ACTIONet archetype filtering/aggregation
 	// To unify redundant archetypes across different levels
 		//unification_results unify_archetypes(sp_mat &G, mat &S_r, mat &archetypes, mat &C_stacked, mat &H_stacked, int minPoints, int minClusterSize, double outlier_threshold, int reduced_dim);
-		unification_results unify_archetypes(mat &S_r, mat &C_stacked, mat &H_stacked, double sensitivity, int normalization_method, double edge_threshold);
+		unification_results unify_archetypes(mat &S_r, mat &C_stacked, mat &H_stacked, double sensitivity);
 		
 	
 	// Main functions to build an interaction network from multi-level archetypal decompositions
