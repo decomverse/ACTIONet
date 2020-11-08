46f0ee3a6a5e047ce977795d92502d88388411bc
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2020-11-07 20:41:35 -0500

    Updated unification
    Former-commit-id: 891ae50ea7c004f3ed2ad29c64f9e3eab72975bd

--- src/ACTIONet/archetype_unification/unification.cc
+++ src/ACTIONet/archetype_unification/unification.cc
@@ -116,112 +116,29 @@ namespace ACTIONet {
 		mat arch_H_stacked = H_stacked * C_norm;
 		mat arch_S_r = S_r * C_norm;
 		
-		mat Wg = trans(arch_S_r) * arch_S_r;		
-		Wg = Wg / max(max(abs(Wg)));
+		mat Wg = cor(arch_S_r);
 		mat Hg = computeFullSim(arch_H_stacked, 1);
-		mat G = Wg % Hg;
+		sp_mat G = sp_mat(Wg % Hg);
 		
-		G.transform( [edge_threshold](double val) { return (val < edge_threshold? 0:val); } );
-
-		if(normalization_type == 1) {
-			G = normalise(G, 1, 0);
+		uvec initial_clusters_uvec(G.n_rows);
+		for (int i = 0; i < G.n_rows; i++) 
+			initial_clusters_uvec(i) = i;
+		
+		vec clusters = signed_cluster(G, sensitivity, initial_clusters_uvec, 0);
+		
+		mat C_arch(clusters.n_elem, max(clusters)+1);
+		for(int i = 0; i <= max(clusters); i++) {
+			uvec idx = find(clusters == i);
+			vec v = zeros(clusters.n_elem);
+			v(idx) = ones(idx.n_elem);
+			C_arch.col(i) = v;
 		}
-		else if(normalization_type == 3) {
-			G = NetEnh(G);						
-		}
-		
-		int dim = std::min((int)G.n_cols, 100);
-		SPA_results res = run_SPA(G, dim);
-		
-		uvec selected_columns = res.selected_columns;
+		uvec selected_clusters = find(1 < sum(C_arch));
+		selected_clusters.print("selected_clusters");
+		C_arch = C_arch.cols(selected_clusters);
+				
+		mat C_unified = C_norm * C_arch;
 
-	
-		printf("Computing lead independent vertex set\n"); fflush(stdout);	
-		mat subG = Hg(selected_columns, selected_columns);
-		subG.diag().zeros();
-		subG = normalise(subG, 1, 0);
-			
-		vec is_selected = zeros(selected_columns.n_elem);
-		for(int i = 0; i < selected_columns.n_elem; i++) {
-			vec N = subG.col(i);
-			vec neighbors_contrib = vec( trans(N) * is_selected );
-			double w = neighbors_contrib(0);
-			if(w <= sensitivity) {
-				//printf("pick %d (N. contrib = %f)\n", i, w);
-				is_selected(i) = 1;
-			} else {
-				//printf("drop %d (N. contri = %f)\n", i, w);
-			}
-		}
-		printf("Selected %d states\n", (int)sum(is_selected));
-
-		uvec idx = find(is_selected == 1);
-		uvec selected_archs = selected_columns(idx);
-
-		mat G_ext = subG;
-		G_ext.diag() = 5.0 * ones(G_ext.n_rows);
-		G_ext = normalise(G_ext, 1, 0);
-		
-		mat C_avg = C_norm.cols(selected_columns) * G_ext;
-		mat C_unified = C_avg.cols(idx);
-
-
-		//mat C_unified = C_norm.cols(selected_columns) * subG.cols(idx);
-		
-		/*
-		mat G_ext = G;
-		G_ext.diag() = 5.0 * ones(G_ext.n_rows);
-		G_ext = normalise(G_ext, 1, 0);
-		
-		mat C_avg = C_norm * G_ext;
-		mat C_unified = C_avg.cols(selected_archs);
-		*/
-		
-		//mat C_unified = C_norm.cols(selected_archs);
-
-
-/*
-		field<vec> res = run_HDBSCAN(G, 10, 5);
-		
-		vec cl = res(0);
-		vec cl_p = res(1);
-		
-		int d = arma::max(cl);
-		mat M = zeros(d, cl.n_elem);		
-		for(int ii = 0; ii < cl.n_elem; ii++) {
-			if(cl(ii) > 0) {
-				M(cl(ii)-1, ii) = cl_p(ii);
-			}
-		}
-		mat C_unified = C_norm * trans(M);
-
-		
-
-		/*
-		mat A = H_stacked * C_unified;
-		mat H_unified = run_simplex_regression(A, H_stacked, false);			
-		*/
-
-/*
-		sp_mat A = sp_mat(G);
-		uvec initial_clusters_uvec(A.n_rows);
-		for (int i = 0; i < A.n_rows; i++) initial_clusters_uvec(i) = i;
-		
-		vec cl = unsigned_cluster(A, sensitivity, initial_clusters_uvec, 0);
-		
-		int d = arma::max(cl);
-		mat M = zeros(d+1, cl.n_elem);		
-		for(int ii = 0; ii < cl.n_elem; ii++) {
-				M(cl(ii), ii) = 1;
-		}
-		vec cc = sum(M, 1);
-		cc.print("cc");
-		
-		uvec idx = find(cc >= 5);
-		
-		mat C_unified = C_norm * trans(M.rows(idx));
-*/
-		
 		C_unified = normalise(C_unified, 1, 0);					
 		
 		mat W_unified = S_r * C_unified;
