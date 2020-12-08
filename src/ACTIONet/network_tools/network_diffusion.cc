cfbaf16cd18813e69e07508eb2df25934c3ac407
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2020-12-07 19:11:16 -0500

    Updated Dockerfile
    Former-commit-id: 165967734fd478e169f9cc313bb47126425c51d7

--- src/ACTIONet/network_tools/network_diffusion.cc
+++ src/ACTIONet/network_tools/network_diffusion.cc
@@ -112,6 +112,109 @@ namespace ACTIONet {
 	}	
 	
 	
+	mat compute_network_diffusion_direct(sp_mat &G, sp_mat &X0, int thread_no = 4, double alpha = 0.85) {		
+		// A common struct that cholmod always needs
+		cholmod_common c ;
+		cholmod_start (&c) ;
+
+		int *Ti, *Tj;
+		double *Tx;
+
+
+		// Construct A
+		// Construct transition matrix
+		vec d = vec(trans(sum(G, 0)));
+		uvec zero_idx = find(d == 0);
+		d(zero_idx).ones();		
+		sp_mat P = G;
+
+		for(sp_mat::iterator it = P.begin(); it != P.end(); ++it) {
+			(*it) = (*it) / (sqrt(d(it.row()) * d(it.col())));
+		}
+		P = -alpha * P;
+		P.diag().ones();
+
+
+		printf("Creating A\n"); fflush(stdout);
+		cholmod_triplet *T = cholmod_allocate_triplet(P.n_rows, P.n_cols, P.n_nonzero, 0, CHOLMOD_REAL, &c);
+		T->nnz = P.n_nonzero;
+		Ti = static_cast<int *>(T->i);
+		Tj = static_cast<int *>(T->j);
+		Tx = static_cast<double *>(T->x);		
+		int idx = 0;
+		for(sp_mat::const_iterator it = P.begin(); it != P.end(); ++it) {
+			Ti[idx] = it.row();
+			Tj[idx] = it.col();
+			Tx[idx] = (*it);			
+			idx++;
+		}		
+		cholmod_sparse *A = cholmod_triplet_to_sparse(T, P.n_nonzero, &c);
+		cholmod_free_triplet(&T, &c);
+
+
+		// Construct B
+		printf("Creating B\n"); fflush(stdout);
+		vec d_X = vec(trans(sum(X0, 0)));
+		zero_idx = find(d_X == 0);
+		d_X(zero_idx).ones();		
+		sp_mat D_X(X0.n_cols, X0.n_cols);
+		D_X.diag() = d_X;			
+
+				
+		X0 = normalise(X0, 1, 0);
+		
+		T = cholmod_allocate_triplet(X0.n_rows, X0.n_cols, X0.n_nonzero, 0, CHOLMOD_REAL, &c);
+		T->nnz = X0.n_nonzero;
+		Ti = static_cast<int *>(T->i);
+		Tj = static_cast<int *>(T->j);
+		Tx = static_cast<double *>(T->x);
+		idx = 0;
+		for(sp_mat::const_iterator it = X0.begin(); it != X0.end(); ++it) {
+			Ti[idx] = it.row();
+			Tj[idx] = it.col();
+			Tx[idx] = (*it);			
+			idx++;
+		}		
+		cholmod_sparse *B = cholmod_triplet_to_sparse(T, X0.n_nonzero, &c);
+		cholmod_free_triplet(&T, &c);
+
+
+		// Solve linear system
+		printf("Chlmod analyze\n"); fflush(stdout);		
+		cholmod_factor *L = cholmod_analyze (A, &c) ;
+		printf("Chlmod factor\n"); fflush(stdout);		
+		cholmod_factorize (A, L, &c) ;
+		printf("Solve\n"); fflush(stdout);		
+		cholmod_sparse *A_inv_B = cholmod_spsolve (CHOLMOD_A, L, B, &c);
+
+		// Export results
+		printf("Export\n"); fflush(stdout);		
+		T = cholmod_sparse_to_triplet(A_inv_B, &c);		
+		Ti = (int*) T->i;
+		Tj = (int*) T->j;
+		Tx = (double *)T->x;			
+		umat locations(2, T->nnz);
+		for(int k = 0; k < T->nnz; k++) {
+			locations(0, k) = Ti[k];
+			locations(1, k) = Tj[k];
+		}
+		mat PR = mat(sp_mat(locations, (1-alpha)*vec(Tx, T->nnz), X0.n_rows, X0.n_cols));
+
+		PR = normalise(PR, 1, 0);		
+		PR = PR * D_X;
+		
+		
+		// Free up matrices
+		cholmod_free_factor (&L, &c) ; 		
+		cholmod_free_sparse (&A, &c) ;
+		cholmod_free_sparse (&B, &c) ;		
+		cholmod_free_triplet(&T, &c);
+		cholmod_finish (&c) ;          
+		
+		
+		return (PR) ;	
+	}
+	
 }
 
 
