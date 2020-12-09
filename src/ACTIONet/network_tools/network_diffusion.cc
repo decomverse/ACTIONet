3a1e3f20c1b4dd5c390cf51b94cf3561c1d1eb47
Sebastian Pineda <sebastianpineda95@gmail.com>
2020-12-08 21:48:04 -0600

    Rewrite main workflow logging

--- src/ACTIONet/network_tools/network_diffusion.cc
+++ src/ACTIONet/network_tools/network_diffusion.cc
@@ -7,7 +7,7 @@
 template<class Function>
 inline void ParallelFor(size_t start, size_t end, size_t numThreads, Function fn) {
     if (numThreads <= 0) {
-        numThreads = std::thread::hardware_concurrency();
+        numThreads = SYS_THREADS_DEF;
     }
 
     if (numThreads == 1) {
@@ -60,61 +60,56 @@ inline void ParallelFor(size_t start, size_t end, size_t numThreads, Function fn
 
 }
 
-namespace ACTIONet {	
+namespace ACTIONet {
 	arma::vec diffusion_solve_FISTA(arma::sp_mat& adj_mat, arma::vec& prob_dist, double alpha, double rho, double epsilon, int max_iter);
 
-	mat PR_linsys(sp_mat &G, sp_mat &X, double alpha = 0.85, int thread_no = -1) {		
+	mat PR_linsys(sp_mat &G, sp_mat &X, double alpha = 0.85, int thread_no = -1) {
 		X = normalise(X, 1, 0);
-		
+
 		/*
 		rowvec d = sum(G, 0);
 		uvec idx = find(c != 0);
 		d[idx] = 1 / d[idx];
-		
-		sp_mat D;		
+
+		sp_mat D;
 		D.diag() = d;
-		
+
 		sp_mat I = speye(size(G));
 		*/
-		sp_mat P = normalise(G, 1, 0);		
+		sp_mat P = normalise(G, 1, 0);
 		sp_mat I = speye(P.n_rows, P.n_cols);
-		sp_mat A = I - alpha*P;		
-		//mat PR = (1-alpha)*spsolve(A, mat(X), "superlu"); 
-		mat PR = (1-alpha)*spsolve(A, mat(X)); 
+		sp_mat A = I - alpha*P;
+		//mat PR = (1-alpha)*spsolve(A, mat(X), "superlu");
+		mat PR = (1-alpha)*spsolve(A, mat(X));
 
-		
+
 		return(PR);
 	}
 
 
-	mat compute_network_diffusion(sp_mat &G, sp_mat &X0, int thread_no = 4, double alpha = 0.85, int max_it = 3) {		
+	mat compute_network_diffusion(sp_mat &G, sp_mat &X0, int thread_no = 4, double alpha = 0.85, int max_it = 3) {
 		thread_no = std::min(thread_no, (int)X0.n_cols);
-		
+
 		int N = G.n_rows;
 		vec z = ones(N);
 		vec c = vec(trans(sum(G, 0)));
 		uvec idx = find(c);
 		z(idx) = ones(idx.n_elem)*(1.0 - alpha);
 		z = z / N;
-		
+
 		sp_mat P = alpha*normalise(G, 1, 0);
 		X0 = normalise(X0, 1, 0);
 		mat X = mat(X0);
-				
-		X0 *= N;			
+
+		X0 *= N;
 		rowvec zt = trans(z);
-		ParallelFor(0, X.n_cols, thread_no, [&](size_t i, size_t threadId) {			
+		ParallelFor(0, X.n_cols, thread_no, [&](size_t i, size_t threadId) {
 			X.col(i) = P*X.col(i) + X0.col(i)*(zt*X.col(i));
 		});
 		//X = normalise(X, 1)
-		
+
 		return(X);
-	}	
-	
-	
+	}
+
+
 }
-
-
-
-
-
--- src/ACTIONet/visualization/layout.cc
+++ src/ACTIONet/visualization/layout.cc
@@ -13,9 +13,9 @@ const double UMAP_A[101] = {1.93280839781719,1.89560586588002,1.85873666431227,1
 const double UMAP_B[101] = {0.790494973419029,0.80063784415826,0.810876441425738,0.821199202674006,0.831595366275022,0.84205539236769,0.852571713401325,0.863135518043442,0.873741680140683,0.884384956993888,0.895060878257082,0.905765637284042,0.916495998501859,0.927249214280422,0.938022954467018,0.948815759038301,0.95962499558526,0.970449732070657,0.981288783823989,0.992141168965973,1.00300608092206,1.01388286515112,1.02477099750548,1.03567006898871,1.04657977025277,1.05749987674998,1.06843023939592,1.07937077470387,1.09032145585694,1.10128169075827,1.11225322117536,1.12323470900213,1.13422639755358,1.14522861434516,1.15624176559097,1.16726633179917,1.17830241385901,1.18934945144456,1.20040819996369,1.21147891097075,1.22256381651844,1.23366041866219,1.24477022428392,1.2558936051142,1.26703094885274,1.27818265467871,1.28934756395537,1.30052872175886,1.31172539107843,1.32293800168803,1.3341669930459,1.34541281413396,1.35667592718974,1.36795680610473,1.37925594017143,1.39057383474783,1.40191101858967,1.41326804557094,1.42464550789942,1.436044048272,1.44746436980037,1.45890393087319,1.47036701291879,1.48185337703821,1.49336326709497,1.50489726618312,1.51645596605121,1.52803997486173,1.53964990048402,1.55128637349183,1.56295003156298,1.57464152150044,1.58636409305622,1.59811350189048,1.60989278253114,1.62170263415549,1.63354377154668,1.64541692037945,1.65732282325244,1.66926223230814,1.68123591907029,1.69324466615879,1.70528927262371,1.71737055545595,1.72948934595558,1.74164649289645,1.75384285823827,1.76607932576738,1.77835679827623,1.79067619009556,1.80303844043406,1.81544450541945,1.82789536263139,1.84039200538657,1.85293545544251,1.86552674229068,1.87816693701183,1.89085711093115,1.90359837758981,1.91638829237987,1.92923479503841};
 
 #define NEGATIVE_SAMPLE_RATE 5.0
-#define LEARNING_RATE 1.0	
-#define UMAP_SEED 0	
-#define GAMMA 1.0 
+#define LEARNING_RATE 1.0
+#define UMAP_SEED 0
+#define GAMMA 1.0
 
 
 std::vector<double>  optimize_layout_umap(
@@ -27,15 +27,15 @@ std::vector<double>  optimize_layout_umap(
     double negative_sample_rate, bool approx_pow,
     std::size_t n_threads, std::size_t grain_size,
     bool move_other);
-    
-    
+
+
 template<class Function>
 inline void ParallelFor(size_t start, size_t end, size_t thread_no, Function fn) {
     if (thread_no <= 0) {
-        thread_no = std::thread::hardware_concurrency();
+        thread_no = SYS_THREADS_DEF;
         printf("# Threads = %d\n", thread_no);
     }
-    
+
 
     if (thread_no == 1) {
         for (size_t id = start; id < end; id++) {
@@ -87,42 +87,42 @@ inline void ParallelFor(size_t start, size_t end, size_t thread_no, Function fn)
 
 }
 
-mat zscore(mat A) {	
+mat zscore(mat A) {
 	rowvec mu = mean(A, 0);
 	rowvec sigma = stddev(A, 0);
 
-	
+
 	for(int j = 0; j < A.n_cols; j++) {
 		A.col(j) = (A.col(j) - mu(j)) / sigma(j);
 	}
-	
+
 	return A;
 }
 
-namespace ACTIONet {	
+namespace ACTIONet {
 
-	sp_mat smoothKNN(sp_mat D, int thread_no = -1) {		
+	sp_mat smoothKNN(sp_mat D, int thread_no = -1) {
 		double epsilon = 1e-6;
 
 		int nV = D.n_cols;
-		sp_mat G = D;			
-		
-		//#pragma omp parallel for num_threads(thread_no) 			
+		sp_mat G = D;
+
+		//#pragma omp parallel for num_threads(thread_no)
 		//for(int i = 0; i < nV; i++) {
 		ParallelFor(0, nV, thread_no, [&](size_t i, size_t threadId) {
 			sp_mat v = D.col(i);
-			vec vals = nonzeros(v);	
+			vec vals = nonzeros(v);
 			if(vals.n_elem > 0) {
-				
+
 				double rho = min(vals);
-				vec negated_shifted_vals = -(vals - rho); 
+				vec negated_shifted_vals = -(vals - rho);
 				double target = log2(vals.n_elem);
-				
+
 				// Binary search to find optimal sigma
 				double sigma = 1.0;
 				double lo = 0.0;
 				double hi = DBL_MAX;
-				
+
 				int j;
 				for(j = 0; j < 64; j ++) {
 					double obj = sum(exp(negated_shifted_vals / sigma));
@@ -143,18 +143,18 @@ namespace ACTIONet {
 						else {
 							sigma = 0.5 * (lo + hi);
 						}
-					}				
+					}
 				}
-				
-				double obj = sum(exp(negated_shifted_vals / sigma));			
+
+				double obj = sum(exp(negated_shifted_vals / sigma));
 				//printf("%d- rho = %.3f, degree = %d, log2(k) = %.2e, sigma = %.2e, residual = %.2e, iters = %d\n", i, rho, vals.n_elem, target, sigma, abs(obj - target), j);
-				
+
 				for(sp_mat::col_iterator it = G.begin_col(i); it != G.end_col(i); ++it) {
 					*it = max(1e-16, exp( -max(0.0, (*it) - rho ) / sigma ));
-				}			
+				}
 			}
 		});
-		
+
 		return(G);
 	}
 
@@ -162,23 +162,23 @@ namespace ACTIONet {
 		mat S_r,
 		int compactness_level = 50,
 		unsigned int n_epochs = 500,
-		int thread_no = 0) { 
+		int thread_no = 0) {
 
-			
+
 		field<mat> res(3);
 
 		mat init_coors = trans(zscore(trans(S_r.rows(0, 2))));
-		
+
 		printf("Running layout with: compactness=%d, # epochs = %d\n", compactness_level, n_epochs);
-		
-		G.for_each( [](sp_mat::elem_type& val) { val = 1.0 - val; } );		
+
+		G.for_each( [](sp_mat::elem_type& val) { val = 1.0 - val; } );
 		G = smoothKNN(G, thread_no);
 
 		unsigned int nV = G.n_rows;
 
 		if(compactness_level < 0 || compactness_level > 100)
 			compactness_level = 50;
-			
+
 		double a_param = UMAP_A[compactness_level];
 		double b_param = UMAP_B[compactness_level];
 
@@ -186,40 +186,40 @@ namespace ACTIONet {
 		unsigned int nE = G.n_nonzero;
 		vector<unsigned int> positive_head(nE);
 		vector<unsigned int> positive_tail(nE);
-		vector<double> epochs_per_sample(nE);		
-	
-	
+		vector<double> epochs_per_sample(nE);
+
+
 		const double* values = G.values;
 		const uword*  rows = G.row_indices;
 		const uword*  col_offsets = G.col_ptrs;
-		
-		
+
+
 		uword* cols = new uword[G.n_nonzero];
 
 		uvec cv(col_offsets, G.n_cols+1);
-		vec delta = diff(conv_to<vec>::from(cv));		
-		vec kk = join_vert(zeros(1), cumsum(delta));		
-		//for(int j = 0; j < nV; j++) {			
-		ParallelFor(0, nV, thread_no, [&](size_t j, size_t threadId) {			
+		vec delta = diff(conv_to<vec>::from(cv));
+		vec kk = join_vert(zeros(1), cumsum(delta));
+		//for(int j = 0; j < nV; j++) {
+		ParallelFor(0, nV, thread_no, [&](size_t j, size_t threadId) {
 			int k = (int)kk[j];
 			int M = (int)delta[j];
 			for(int l = 0; l < M; l++) {
 				cols[k+l] = j;
 			}
 		});
-		
+
 		double w_max = arma::max(vec(values, G.n_nonzero));
 		ParallelFor(0, nE, thread_no, [&](size_t i, size_t threadId) {
 			epochs_per_sample[i] = w_max / values[i]; // Higher the weight of the edge, the more likely it is to be sampled (inversely proportional to epochs_per_sample)
 			positive_head[i] = rows[i];
 			positive_tail[i++] = cols[i];
-			
+
 		});
 
 
 
 
-       		
+
 		/*
 		int i = 0;
 		double w_max = max(max(G));
@@ -227,56 +227,56 @@ namespace ACTIONet {
 			epochs_per_sample[i] = w_max / (*it); // Higher the weight of the edge, the more likely it is to be sampled (inversely proportional to epochs_per_sample)
 			positive_head[i] = it.row();
 			positive_tail[i++] = it.col();
-		}		
+		}
 		*/
-		
+
 		// Initial coordinates of vertices (0-simplices)
 		mat initial_coor2D = init_coors.rows(0, 1);
-		vector<double> head_vec(initial_coor2D.memptr(), initial_coor2D.memptr()+initial_coor2D.n_elem);		
+		vector<double> head_vec(initial_coor2D.memptr(), initial_coor2D.memptr()+initial_coor2D.n_elem);
 		vector<double> tail_vec(head_vec);
-		
-		
-		printf("Computing 2D layout ... "); fflush(stdout);		
+
+
+		printf("Computing 2D layout ... "); fflush(stdout);
 		// Stores linearized coordinates [x1, y1, x2, y2, ...]
 		vector<double> result;
 
 		result = optimize_layout_umap(head_vec, tail_vec, positive_head, positive_tail, n_epochs,
-			nV, epochs_per_sample, a_param, b_param, GAMMA, LEARNING_RATE, NEGATIVE_SAMPLE_RATE, false, thread_no, 1, true);	
-		
+			nV, epochs_per_sample, a_param, b_param, GAMMA, LEARNING_RATE, NEGATIVE_SAMPLE_RATE, false, thread_no, 1, true);
+
 		mat coordinates(result.data(), 2, nV);
 		//coordinates = robust_zscore(trans(coordinates));
 		coordinates = trans(coordinates);
-		
-		printf("Done\n"); fflush(stdout);
 
-		
+		printf("done\n"); fflush(stdout);
+
+
 		/****************************
 		 *  Compute 3D Embedding	*
-		 ***************************/	
+		 ***************************/
 		mat initial_coor3D = join_vert(trans(zscore(coordinates)), init_coors.row(2));
-		
-		head_vec.clear(); 
+
+		head_vec.clear();
 		head_vec.resize(initial_coor3D.n_elem);
-		std::copy(initial_coor3D.memptr(), initial_coor3D.memptr() + initial_coor3D.n_elem, head_vec.begin());		
-		tail_vec = head_vec;		
-		
-		
+		std::copy(initial_coor3D.memptr(), initial_coor3D.memptr() + initial_coor3D.n_elem, head_vec.begin());
+		tail_vec = head_vec;
+
+
 		printf("Compute 3D layout ... "); fflush(stdout);
 		result.clear();
 		result = optimize_layout_umap(head_vec, tail_vec, positive_head, positive_tail, n_epochs,
-			nV, epochs_per_sample, a_param, b_param, GAMMA, LEARNING_RATE, NEGATIVE_SAMPLE_RATE, false, thread_no, 1, true);	
-		
-		mat coordinates_3D(result.data(), 3, nV);	
+			nV, epochs_per_sample, a_param, b_param, GAMMA, LEARNING_RATE, NEGATIVE_SAMPLE_RATE, false, thread_no, 1, true);
+
+		mat coordinates_3D(result.data(), 3, nV);
 		//coordinates_3D = robust_zscore(trans(coordinates_3D));
 		coordinates_3D = trans(coordinates_3D);
-		
-		printf("Done\n"); fflush(stdout);
-			
-	  
-	  
+
+		printf("done\n"); fflush(stdout);
+
+
+
 		/****************************
 		 *  Now compute node colors *
-		 ***************************/	
+		 ***************************/
 		printf("Estimating node colors ... \n"); fflush(stdout);
 
 		/*
@@ -284,25 +284,25 @@ namespace ACTIONet {
 		mat score;
 		vec latent;
 		vec tsquared;
-		princomp(coeff, score, latent, tsquared, coordinates_3D);		
+		princomp(coeff, score, latent, tsquared, coordinates_3D);
 		mat Z = robust_zscore(score);
 */
-		
+
 		mat U;
 		vec s;
 		mat V;
-		svd_econ(U, s, V, coordinates_3D);		
+		svd_econ(U, s, V, coordinates_3D);
 		mat Z = zscore(U);
 
 
 /*
  		mat Ct = trans(coordinates_3D);
-		ReducedKernel reduction = reduce_kernel(Ct, 3, 1000, 1, -1, 0, false);				
+		ReducedKernel reduction = reduce_kernel(Ct, 3, 1000, 1, -1, 0, false);
 		printf("Coor3D Sr: %d x %d\n", reduction.S_r.n_rows, reduction.S_r.n_cols);
-		
+
 		mat Z = zscore(trans(reduction.S_r));
 */
-			
+
 		vec a = 75*Z.col(0);
 		vec b = 75*Z.col(1);
 
@@ -312,21 +312,21 @@ namespace ACTIONet {
 		double r_channel, g_channel, b_channel;
 		mat RGB_colors = zeros(nV, 3);
 		for(int i = 0; i < nV; i++) {
-			Lab2Rgb(&r_channel, &g_channel, &b_channel, L(i), a(i), b(i));			
+			Lab2Rgb(&r_channel, &g_channel, &b_channel, L(i), a(i), b(i));
 
 			RGB_colors(i, 0) = min(1.0, max(0.0, r_channel));
 			RGB_colors(i, 1) = min(1.0, max(0.0, g_channel));
-			RGB_colors(i, 2) = min(1.0, max(0.0, b_channel));			
+			RGB_colors(i, 2) = min(1.0, max(0.0, b_channel));
 		}
-		
+
 		printf("done\n");
-		
+
 		res(0) = coordinates;
 		res(1) = coordinates_3D;
 		res(2) = RGB_colors;
-		
-		return res;	  		
-	}	
+
+		return res;
+	}
 
 
 
@@ -337,23 +337,23 @@ namespace ACTIONet {
 		mat coor3D,
 		mat colRGB,
 		int compactness_level = 50,
-		unsigned int n_epochs = 500, 
-		int thread_no = 8) { 
+		unsigned int n_epochs = 500,
+		int thread_no = 8) {
 
 		int nV = W.n_cols;
-		
+
 		if(compactness_level < 0 || compactness_level > 100)
 			compactness_level = 50;
-			
+
 		double a_param = UMAP_A[compactness_level];
 		double b_param = UMAP_B[compactness_level];
-		
+
 		// 1) Smooth similarity scores
 		sp_mat G = W;
-		G.for_each( [](sp_mat::elem_type& val) { val = 1 / val; } );		
+		G.for_each( [](sp_mat::elem_type& val) { val = 1 / val; } );
 		W = smoothKNN(G, thread_no);
-		
-		
+
+
 		// 2) Use prior embedding to initialize new points (after smoothing -- might need further thresholding prior to averaging)
 		sp_mat Wn = normalise(W, 1, 0); // soft-max
 		mat arch_coor2D = mat(coor2D * Wn);
@@ -362,26 +362,26 @@ namespace ACTIONet {
 		arch_colRGB = clamp(arch_colRGB, 0, 1);
 
 		// 3) Store old (ACTIONet) and new (initial) coordinates
-		vector<double> head_vec(arch_coor2D.memptr(), arch_coor2D.memptr()+arch_coor2D.n_elem);		
-		vector<double> tail_vec(coor2D.memptr(), coor2D.memptr()+coor2D.n_elem);		
+		vector<double> head_vec(arch_coor2D.memptr(), arch_coor2D.memptr()+arch_coor2D.n_elem);
+		vector<double> tail_vec(coor2D.memptr(), coor2D.memptr()+coor2D.n_elem);
 
 
 
-		// 4) Linearize edges and estimate epochs 
+		// 4) Linearize edges and estimate epochs
 		double global_max = arma::max(arma::max(W));
 		double min_threshold = global_max / n_epochs;
 		vector<unsigned int> positive_head; positive_head.reserve(W.n_nonzero);
-		vector<unsigned int> positive_tail; positive_tail.reserve(W.n_nonzero);	
-		vector<double> epochs_per_sample; epochs_per_sample.reserve(W.n_nonzero);		
-		
+		vector<unsigned int> positive_tail; positive_tail.reserve(W.n_nonzero);
+		vector<double> epochs_per_sample; epochs_per_sample.reserve(W.n_nonzero);
+
 		sp_mat::const_iterator it     = W.begin();
 		sp_mat::const_iterator it_end = W.end();
-		for(; it != it_end; ++it) {		
+		for(; it != it_end; ++it) {
 			double w = (*it);
 			if( w < min_threshold) {
 				continue;
 			}
-			
+
 			epochs_per_sample.push_back( (double) (global_max / w) );
 			positive_tail.push_back(it.row());
 			positive_head.push_back(it.col());
@@ -391,45 +391,40 @@ namespace ACTIONet {
 		vector<double> result;
 
 		result = optimize_layout_umap(head_vec, tail_vec, positive_head, positive_tail, n_epochs,
-			nV, epochs_per_sample, a_param, b_param, GAMMA, LEARNING_RATE / 4.0, NEGATIVE_SAMPLE_RATE, false, thread_no, 1, false);	
+			nV, epochs_per_sample, a_param, b_param, GAMMA, LEARNING_RATE / 4.0, NEGATIVE_SAMPLE_RATE, false, thread_no, 1, false);
 
-		
+
 		mat coordinates(result.data(), 2, nV);
-		printf("Done\n"); fflush(stdout);
-		
-		
-		
+		printf("done\n"); fflush(stdout);
 
-		vector<double> head_vec3D(coor3D.memptr(), coor3D.memptr()+coor3D.n_elem);		
-		vector<double> tail_vec3D(arch_coor3D.memptr(), arch_coor3D.memptr()+arch_coor3D.n_elem);	
-		
+
+
+
+		vector<double> head_vec3D(coor3D.memptr(), coor3D.memptr()+coor3D.n_elem);
+		vector<double> tail_vec3D(arch_coor3D.memptr(), arch_coor3D.memptr()+arch_coor3D.n_elem);
+
 		printf("Compute 3D layout ... "); fflush(stdout);
 		result.clear();
 
 
 		result = optimize_layout_umap(head_vec, tail_vec, positive_head, positive_tail, n_epochs,
-			nV, epochs_per_sample, a_param, b_param, GAMMA, LEARNING_RATE / 4.0, NEGATIVE_SAMPLE_RATE, true, thread_no, 1, false);	
+			nV, epochs_per_sample, a_param, b_param, GAMMA, LEARNING_RATE / 4.0, NEGATIVE_SAMPLE_RATE, true, thread_no, 1, false);
 
-		mat coordinates_3D(result.data(), 3, nV);	
-		printf("Done\n"); fflush(stdout);
-	  
+		mat coordinates_3D(result.data(), 3, nV);
+		printf("done\n"); fflush(stdout);
+
 
 
 		field<mat> res(3);
 
-		
+
 		res(0) = coordinates;
 		res(1) = coordinates_3D;
 		res(2) = arch_colRGB;
-		
-		return res;	  		
-	}	
+
+		return res;
+	}
 
 
-	
+
 }
-
-
-
-
-
