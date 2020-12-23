701b2a07f570b3357a9f56081c22e3b84d6dac40
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2020-12-23 12:08:33 -0500

    testing

--- python_interface/wrapper.cc
+++ python_interface/wrapper.cc
@@ -160,7 +160,7 @@ py::dict reduce_kernel_full(arma::Mat<npdouble> &S, int reduced_dim = 50,
   }
 
   // Armadillo employs delayed evaluation, but pybind11 doesn't know that.
-  mat S_r = trans(V).eval());
+  mat S_r = trans(V).eval();
   S_r = round(S_r*1000)/1000.0;
   res["S_r"] = S_r;
 
