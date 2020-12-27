efc1ceec8e173d1a9517f32454f39785ec89a284
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2020-12-27 11:29:34 -0500

    rounded ACTION results

--- python_interface/wrapper.cc
+++ python_interface/wrapper.cc
@@ -288,17 +288,19 @@ py::dict run_ACTION(arma::Mat<npdouble> &S_r, int k_min = 2, int k_max = 30,
 
   py::list C(k_max);
   for (int i = 0; i < k_max; i++) {
-    C[i] = trace.C[i + 1];
-    C[i] = clamp(C[i], 1e-5, 1);
-	C[i] = normalise(C[i], 1);
+	mat curr_C = trace.C[i + 1];
+    curr_C = clamp(curr_C, 1e-5, 1);
+	curr_C = normalise(curr_C, 1);
+    C[i] = curr_C;
   }
   res["C"] = C;
 
   py::list H(k_max);
   for (int i = 0; i < k_max; i++) {
-    H[i] = trace.H[i + 1];
-    H[i] = clamp(H[i], 1e-5, 1);
-	H[i] = normalise(H[i], 1);
+	mat curr_H = trace.H[i + 1];
+    curr_H = clamp(curr_H, 1e-5, 1);
+	curr_H = normalise(curr_H, 1);
+    H[i] = curr_H;
   }
   res["H"] = H;
 
