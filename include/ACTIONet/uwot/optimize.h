856ef4a40a411ee2f4dbaca4ef3894da666c5c69
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2021-01-16 11:58:07 -0500

    changes

--- include/ACTIONet/uwot/optimize.h
+++ include/ACTIONet/uwot/optimize.h
@@ -121,7 +121,7 @@ struct SgdWorker {
 
       float dist_squared = 0.0;
       for (std::size_t d = 0; d < ndim; d++) {
-        float diff = head_embedding[dj + d] - tail_embedding[dk + d];
+        float diff = uniform_dist(rng)/tail_nvert; //head_embedding[dj + d] - tail_embedding[dk + d];
 		if(i < 50) {
 			printf("%d- <%d, %d> -> dim%d-- <%f, %f> -> diff = %e\n", i+1, dj+1, dk+1, head_embedding[dj + d], d+1, tail_embedding[dk + d], diff);
 		}
@@ -137,7 +137,7 @@ struct SgdWorker {
 	  
 	  g1 += grad_coeff;
       for (std::size_t d = 0; d < ndim; d++) {
-        float grad_d = alpha * clamp(1 * dys[d], Gradient::clamp_lo,
+        float grad_d = alpha * clamp(grad_coeff * dys[d], Gradient::clamp_lo,
                                      Gradient::clamp_hi);
         head_embedding[dj + d] += grad_d;
 		//if(DoMoveVertex)
@@ -158,7 +158,7 @@ struct SgdWorker {
         }
         float dist_squared = 0.0;
         for (std::size_t d = 0; d < ndim; d++) {
-          float diff = head_embedding[dj + d] - tail_embedding[dkn + d];
+          float diff = uniform_dist(rng)/tail_nvert; //head_embedding[dj + d] - tail_embedding[dkn + d];
           dys[d] =  diff;
           dist_squared += diff * diff;
         }
@@ -171,7 +171,7 @@ struct SgdWorker {
 			printf("%d- <%d, %d> (-) ->  dist_squared=%e, grad_coeff = %e\n", i+1, dj+1, dk+1, dist_squared, grad_coeff);
 
         for (std::size_t d = 0; d < ndim; d++) {
-          float grad_d = alpha * clamp(1 * dys[d], Gradient::clamp_lo, Gradient::clamp_hi);
+          float grad_d = alpha * clamp(grad_coeff * dys[d], Gradient::clamp_lo, Gradient::clamp_hi);
 
           head_embedding[dj + d] += grad_d;
           g3 += grad_d;
