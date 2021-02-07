c005aa495a692f5d184f6eaf7c1122847366c6da
Sebastian Pineda <sebastianpineda95@gmail.com>
2021-02-06 20:04:04 -0600

    re-merge

--- R/show_ace.R
+++ R/show_ace.R
@@ -5,16 +5,15 @@
 #' @export
 #' @importMethodsFrom SummarizedExperiment show
 setMethod("show", "ACTIONetExperiment", function(object) {
-    
+
     validObject(object)
-    
+
     callNextMethod()
-    
-    cat("rowMaps(", (length(rowMaps(object, all = F))), "): ", paste(names(rowMaps(object, 
-        all = F)), collapse = " "), "\n", "colMaps(", (length(colMaps(object, all = F))), 
-        "): ", paste(names(colMaps(object, all = F)), collapse = " "), "\n", "rowNets(", 
-        (length(rowNets(object))), "): ", paste(names(rowNets(object)), collapse = " "), 
-        "\n", "colNets(", (length(colNets(object))), "): ", paste(names(colNets(object)), 
-            collapse = " "), "\n", sep = "")
-    
+
+    cat(
+      "rowMaps(", (length(rowMaps(object, all = F))), "): ", paste(names(rowMaps(object, all = F)), collapse = " "), "\n",
+      "colMaps(", (length(colMaps(object, all = F))), "): ", paste(names(colMaps(object, all = F)), collapse = " "), "\n",
+      "rowNets(", (length(rowNets(object))), "): ", paste(names(rowNets(object)), collapse = " "), "\n",
+      "colNets(", (length(colNets(object))), "): ", paste(names(colNets(object)), collapse = " "), "\n", sep = "")
+
 })
