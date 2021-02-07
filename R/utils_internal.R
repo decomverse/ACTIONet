3f79a5e0db59c3a56d6eef8d970d05d828f9339b
Sebastian Pineda <sebastianpineda95@gmail.com>
2021-02-06 19:14:30 -0600

    Fix unification
    Former-commit-id: 7817818c0a740c5f1ea608d94227fa5321bb3580

--- R/utils_internal.R
+++ R/utils_internal.R
@@ -161,3 +161,15 @@
   A = Matrix::t(scale(Matrix::t(A), center = center, scale = scale))
   return(A)
 }
+
+.preprocess_design_matrix_and_var_names <- function(design_mat, variable_name = NULL){
+  if (is.null(variable_name)) {
+    variable_name = colnames(design_mat)[ncol(design_mat)]
+  }
+  vn_idx = which(variable_name == colnames(design_mat))[1]
+  colnames(design_mat) = make.names(colnames(design_mat), unique = TRUE, allow_ = FALSE)
+  variable_name = colnames(design_mat)[vn_idx]
+
+  out = list(design_mat = design_mat, variable_name = variable_name_new)
+  return(out)
+}
