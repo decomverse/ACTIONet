fd394338beb909c3f9d1fdbadfb12b3ee9ca6003
Anna Shcherbina <annashch@insitro.com>
2021-02-19 17:57:15 +0000

    testing network construction

--- tests/test_network_construction.py
+++ tests/test_network_construction.py
@@ -11,9 +11,13 @@ adata=sc.read_h5ad("CellSIUS_Wegmann19/normalized_input.h5ad")
 an.pp.reduce_kernel(adata)
 
 #build network
+print("JSD/KNN")
 an_jsd_knn=an.run_ACTIONet(adata,distance_metric="jsd",nn_approach="knn",copy=True)
+print("JSD/K*NN")
 an_jsd_kstarnn=an.run_ACTIONet(adata,distance_metric="jsd",nn_approach="k*nn",copy=True)
+print("L2/K*NN") 
 an_l2_kstarnn=an.run_ACTIONet(adata,distance_metric="l2",nn_approach="k*nn",copy=True)
+print("IP/K*NN") 
 an_ip_kstarnn=an.run_ACTIONet(adata,distance_metric="ip",nn_approach="k*nn",copy=True)
 
 
