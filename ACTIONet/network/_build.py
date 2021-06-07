afb30d84eeb775dcb2228ae0d131c1f67812f647
Anna Shcherbina <annashch@insitro.com>
2021-06-07 10:07:20 +0000

    testing docstring position

--- ACTIONet/network/_build.py
+++ ACTIONet/network/_build.py
@@ -4,40 +4,39 @@ from anndata import AnnData
 
 import _ACTIONet as _an
 
-def build_network(
+"""\
+Build ACTIIONet 
+
+Computes and returns the ACTIONet graph
+Parameters
+----------
+adata
+    Current AnnData object storing the ACTIONet results    
+density
+    Controls the overall density of constructed network.
+    Larger values results in more retained edges.
+n_threads
+    Number of parallel threads used for identifying nearest-neighbors.
+    Defaults to available threads on the machine.
+mutual_edges_only
+    Whether to return only edges that there is a bi-directional/mutual relationship
+copy
+    Return a copy instead of writing to adata.
+
+Returns
+-------
+    adata : anndata.AnnData
+    if `copy=True` returns None or else adds fields to `adata`:
+
+    `.obsp['ACTIONet']`
+    `.uns['ACTIONet']['params']`
+"""def build_network(
     adata: AnnData,
     density: Optional[float] = 1.0,
     n_threads: Optional[int] = 0,
     mutual_edges_only: Optional[bool] = True,
     copy: Optional[bool] = False        
 ) -> AnnData:
-    """\
-    Build ACTIIONet 
-    
-    Computes and returns the ACTIONet graph
-    Parameters
-    ----------
-    adata
-        Current AnnData object storing the ACTIONet results    
-    density
-        Controls the overall density of constructed network.
-        Larger values results in more retained edges.
-    n_threads
-        Number of parallel threads used for identifying nearest-neighbors.
-        Defaults to available threads on the machine.
-    mutual_edges_only
-        Whether to return only edges that there is a bi-directional/mutual relationship
-    copy
-        Return a copy instead of writing to adata.
-
-    Returns
-    -------
-        adata : anndata.AnnData
-        if `copy=True` returns None or else adds fields to `adata`:
-
-        `.obsp['ACTIONet']`
-        `.uns['ACTIONet']['params']`
-    """
     if 'ACTION_H_stacked' not in adata.obsm.keys():
         raise ValueError(
             'Did not find adata.obsm[\'ACTION_H_stacked\']. '
