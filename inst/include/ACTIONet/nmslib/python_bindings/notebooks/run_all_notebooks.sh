e423233730e76a30d05f8357c527a466d6f9b2eb
sebastianpineda <sebastianpineda95@gmail.com>
2021-06-07 16:23:25 -0700

    purge headers

--- inst/include/ACTIONet/nmslib/python_bindings/notebooks/run_all_notebooks.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for fn in *.ipynb ; do 
-  echo "Running $fn"
-  `which ipython` $fn
-  echo "===================="
-done
--- inst/include/ACTIONet/nmslib/query_server/thrift_gen.sh
+++
@@ -1,6 +0,0 @@
-thrift --gen java protocol.thrift
-thrift --gen cpp  protocol.thrift
-thrift --gen py  protocol.thrift
-
-
-
