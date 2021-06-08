ac2a7cbc4afb94352b23053728a49d251856a207
Anna Shcherbina <annashch@insitro.com>
2021-06-08 01:21:04 +0000

    removing format_code.sh

--- format_code.sh
+++
@@ -1,3 +0,0 @@
-#clang-format -style=google -dump-config > .clang-format
-#find . \( -path './include/ACTIONet/leiden/*' -o -path './src/ACTIONet/network_tools/leiden/*' -o -path './include/ACTIONet/boost/*' \) -prune -o -type f -regex '.*\.\(cc\|c\|h\)' -exec clang-format -style=file -i {} \;
-#black python_interface/
