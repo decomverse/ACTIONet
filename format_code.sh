a71aee63f638ec7b087f90f9f7f2004ddf491223
Shahin Mohammadi <mohammadi@insitro.com>
2021-07-19 08:25:56 -0700

    Updated metafiles

--- format_code.sh
+++ format_code.sh
@@ -1,3 +1,2 @@
-# clang-format -style=google -dump-config > .clang-format
-# find . \( -path './inst/include/ACTIONet/leiden/*' -o -path './src/ACTIONet/network_tools/leiden/*' -o -path './inst/include/ACTIONet/boost/*' \) -prune -o -type f -regex '.*\.\(cc\|c\|h\)' -exec clang-format -style=file -i {} \;
-# Rscript -e "formatR::tidy_dir('`pwd`/R', recursive = T)"
+clang-format -style=google -dump-config > .clang-format
+find . \( -path './include/' -o -path './src/' \) -prune -o -type f -regex '.*\.\(cpp\|cc\|c\|h\|hpp\)' -exec clang-format -style=file -i {} \;
