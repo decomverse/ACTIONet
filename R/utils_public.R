3548d1f476fea70350a1409f51464f0752245df9
Shahin Mohammadi <mohammadi@insitro.com>
2021-07-19 08:21:45 -0700

    Rebased all C/C++ codes -- undebugged

--- R/utils_public.R
+++
@@ -1,8 +0,0 @@
-#' @export
-orthoProject <- function(A, S) {
-    A = scale(A)
-    S = scale(S)
-    A_r = A - S %*% MASS::ginv(t(S) %*% S) %*% (t(S) %*% A)
-    A_r = scale(A_r)
-    return(A_r)
-}
