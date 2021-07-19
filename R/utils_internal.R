eb2208ab587f4bb8d047f3f36fd31faeb88b604a
Shahin Mohammadi <mohammadi@insitro.com>
2021-07-19 07:27:41 -0700

    ACE has been purged from the repo

--- R/utils_internal.R
+++ R/utils_internal.R
@@ -7,6 +7,47 @@
     }
 }
 
+.preprocess_annotation_features <- function(ace, features_use = NULL) {
+
+    if (is.null(features_use)) {
+        features_use = rownames(ace)
+    } else {
+        features_use = .get_attr_or_split_idx(ace, attr = features_use, return_vec = TRUE,
+            d = 1)
+    }
+    return(features_use)
+}
+
+.preprocess_annotation_labels <- function(labels, ace = NULL) {
+    if (is.null(labels)) {
+        return(NULL)
+    }
+
+    if (is.character(labels)) {
+        if (length(labels) == 1)
+          labels = .get_attr_or_split_idx(ace, attr = labels, return_vec = TRUE)
+        else
+          labels = factor(labels)
+    }
+
+    if ((length(labels) > 1) & is.logical(labels)) {
+        labels = factor(as.numeric(labels), levels = c(0, 1), labels = c("No", "Yes"))
+    }
+
+    if (is.factor(labels)) {
+        v = as.numeric(labels)
+        names(v) = levels(labels)[v]
+        labels = v
+    }
+    if (is.matrix(labels)) {
+        L = as.numeric(labels)
+        names(L) = names(labels)
+        labels = L
+    }
+
+    if (is.null(names(labels)) | length(unique(names(labels))) > 100) {
+        names(labels) = as.character(labels)
+    }
 
     return(labels)
 }
