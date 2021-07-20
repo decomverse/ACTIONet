c80b7ad0f2fd6b5edf1e59ec79c2bf1cd3c135fb
Shahin Mohammadi <mohammadi@insitro.com>
2021-07-20 11:12:48 -0700

    Added necessary include files
    Former-commit-id: c07523869f5e72801e3be23fbb118f555bdba8a6

---
+++ inst/RCpp_util.h
@@ -0,0 +1,17 @@
+#ifndef RCPP_UTIL_H
+#define RCPP_UTIL_H
+
+namespace ACTIONet {
+template <typename T>
+Rcpp::NumericVector arma2vec(const T &x);
+arma::vec roll_var(arma::vec &X);
+Rcpp::NumericVector fast_row_sums(SEXP &A);
+Rcpp::NumericVector fast_column_sums(SEXP &A);
+Rcpp::NumericVector computeSparseRowVariances(Rcpp::IntegerVector j,
+                                              Rcpp::NumericVector val,
+                                              Rcpp::NumericVector rm, int n);
+arma::sp_mat bind_sparse_mats(sp_mat &A, sp_mat &B, int dim);
+}  // namespace ACTIONet
+
+
+#endif
