ffacb15c43a841dd391a27d70489a3b7d8f33eb3
Shahin Mohammadi <mohammadi@insitro.com>
2021-07-21 05:57:24 -0700

    Updated header dependencies and exported names
    Former-commit-id: 031a86b8daf4f42472cdeca78bda7c257268654c

---
+++ inst/include/ACTIONet.h
@@ -0,0 +1,13 @@
+#ifndef ACTIONet_H
+#define ACTIONet_H
+
+#include <Rinterface.h>
+#include <base.h>
+
+#define stdout_printf Rprintf
+#define stderr_printf REprintf
+#define stderr_stop stop
+#define FLUSH R_FlushConsole()
+
+
+#endif
