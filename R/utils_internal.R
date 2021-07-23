c610931def16684dd7ba4c843fc122b659666c73
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2021-07-22 23:08:48 -0700

    Added back .get_attr_or_split_idx
    Former-commit-id: fc3666d962bedd11300bd5ea9b1037cb7b952df6

--- R/utils_internal.R
+++ R/utils_internal.R
@@ -1,19 +1,63 @@
 .check_and_load_package <- function(pkg_names) {
     for (pk in pkg_names) {
         if (!require(pk, character.only = T)) {
-            err = sprintf("Package '%s' is not installed.\n", pk)
+            err <- sprintf("Package '%s' is not installed.\n", pk)
             stop(err)
         }
     }
 }
 
-.preprocess_annotation_features <- function(ace, features_use = NULL) {
-
-    if (is.null(features_use)) {
-        features_use = rownames(ace)
+.get_attr_or_split_idx <- function(ace, attr, groups_use = NULL, return_vec = FALSE,
+                                   d = 2) {
+    if (length(attr) == 1) {
+        split_vec <- switch(d,
+            SummarizedExperiment::rowData(ace)[[attr]],
+            SummarizedExperiment::colData(ace)[[attr]]
+        )
     } else {
-        features_use = .get_attr_or_split_idx(ace, attr = features_use, return_vec = TRUE,
-            d = 1)
+        if (length(attr) != dim(ace)[d]) {
+            err <- sprintf("'attr' length does not match %s of ace.\n", ifelse(d ==
+                1, "NROW", "NCOL"))
+            stop(err)
+        }
+        split_vec <- attr
+    }
+
+    if (is.null(split_vec)) {
+        stop(sprintf("Invalid split conditions.\n"))
+    } else {
+        split_vec <- as.character(split_vec)
+    }
+
+    idx <- 1:dim(ace)[d]
+
+    if (!is.null(groups_use)) {
+        sub_idx <- which(split_vec %in% groups_use)
+        split_vec <- split_vec[sub_idx]
+        if (is.null(split_vec)) {
+              stop(sprintf("Invalid split conditions.\n"))
+          }
+        IDX_out <- split(idx[sub_idx], split_vec)
+        return(IDX_out)
+    } else {
+        IDX_out <- split(idx, split_vec)
+    }
+
+    if (return_vec) {
+        return(split_vec)
+    } else {
+        return(IDX_out)
+    }
+}
+
+.preprocess_annotation_features <- function(ace, features_use = NULL) {
+    if (is.null(features_use)) {
+        features_use <- rownames(ace)
+    } else {
+        features_use <- .get_attr_or_split_idx(ace,
+            attr = features_use, return_vec = TRUE,
+            d = 1
+        )
     }
     return(features_use)
 }
@@ -24,47 +68,48 @@
     }
 
     if (is.character(labels)) {
-        if (length(labels) == 1)
-          labels = .get_attr_or_split_idx(ace, attr = labels, return_vec = TRUE)
-        else
-          labels = factor(labels)
+        if (length(labels) == 1) {
+              labels <- .get_attr_or_split_idx(ace, attr = labels, return_vec = TRUE)
+          } else {
+              labels <- factor(labels)
+          }
     }
 
     if ((length(labels) > 1) & is.logical(labels)) {
-        labels = factor(as.numeric(labels), levels = c(0, 1), labels = c("No", "Yes"))
+        labels <- factor(as.numeric(labels), levels = c(0, 1), labels = c("No", "Yes"))
     }
 
     if (is.factor(labels)) {
-        v = as.numeric(labels)
-        names(v) = levels(labels)[v]
-        labels = v
+        v <- as.numeric(labels)
+        names(v) <- levels(labels)[v]
+        labels <- v
     }
     if (is.matrix(labels)) {
-        L = as.numeric(labels)
-        names(L) = names(labels)
-        labels = L
+        L <- as.numeric(labels)
+        names(L) <- names(labels)
+        labels <- L
     }
 
     if (is.null(names(labels)) | length(unique(names(labels))) > 100) {
-        names(labels) = as.character(labels)
+        names(labels) <- as.character(labels)
     }
 
     return(labels)
 }
 
-.tscalet <- function(A, center = TRUE, scale = TRUE){
-  A = Matrix::t(scale(Matrix::t(A), center = center, scale = scale))
-  return(A)
+.tscalet <- function(A, center = TRUE, scale = TRUE) {
+    A <- Matrix::t(scale(Matrix::t(A), center = center, scale = scale))
+    return(A)
 }
 
-.preprocess_design_matrix_and_var_names <- function(design_mat, variable_name = NULL){
-  if (is.null(variable_name)) {
-    variable_name = colnames(design_mat)[ncol(design_mat)]
-  }
-  vn_idx = which(variable_name == colnames(design_mat))[1]
-  colnames(design_mat) = make.names(colnames(design_mat), unique = TRUE, allow_ = FALSE)
-  variable_name = colnames(design_mat)[vn_idx]
+.preprocess_design_matrix_and_var_names <- function(design_mat, variable_name = NULL) {
+    if (is.null(variable_name)) {
+        variable_name <- colnames(design_mat)[ncol(design_mat)]
+    }
+    vn_idx <- which(variable_name == colnames(design_mat))[1]
+    colnames(design_mat) <- make.names(colnames(design_mat), unique = TRUE, allow_ = FALSE)
+    variable_name <- colnames(design_mat)[vn_idx]
 
-  out = list(design_mat = design_mat, variable_name = variable_name)
-  return(out)
+    out <- list(design_mat = design_mat, variable_name = variable_name)
+    return(out)
 }
