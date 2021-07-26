c69325eae4c262e63d8b29a2cf4138ecc88c0011
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2021-07-26 06:31:15 -0700

    Updated make
    Former-commit-id: d830e84bb488af06018e7d54414fd7060765692b

--- inst/include/RCpp_util.h
+++ inst/include/RCpp_util.h
@@ -1,17 +1,24 @@
 #ifndef RCPP_UTIL_H
 #define RCPP_UTIL_H
 
-namespace ACTIONet {
-template <typename T>
-Rcpp::NumericVector arma2vec(const T &x);
-arma::vec roll_var(arma::vec &X);
-Rcpp::NumericVector fast_row_sums(SEXP &A);
-Rcpp::NumericVector fast_column_sums(SEXP &A);
-Rcpp::NumericVector computeSparseRowVariances(Rcpp::IntegerVector j,
-                                              Rcpp::NumericVector val,
-                                              Rcpp::NumericVector rm, int n);
-arma::sp_mat bind_sparse_mats(sp_mat &A, sp_mat &B, int dim);
-}  // namespace ACTIONet
+#include <RcppArmadillo.h>
 
+#define stdout_printf Rprintf
+#define stderr_printf REprintf
+#define stderr_stop stop
+#define FLUSH R_FlushConsole()
+
+namespace ACTIONet
+{
+    template <typename T>
+    Rcpp::NumericVector arma2vec(const T &x);
+    arma::vec roll_var(arma::vec &X);
+    Rcpp::NumericVector fast_row_sums(SEXP &A);
+    Rcpp::NumericVector fast_column_sums(SEXP &A);
+    Rcpp::NumericVector computeSparseRowVariances(Rcpp::IntegerVector j,
+                                                  Rcpp::NumericVector val,
+                                                  Rcpp::NumericVector rm, int n);
+    arma::sp_mat bind_sparse_mats(sp_mat &A, sp_mat &B, int dim);
+} // namespace ACTIONet
 
 #endif
