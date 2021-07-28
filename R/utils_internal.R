718af76babc1bc5a95c6118d714e670a5a1cddf5
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2021-07-27 22:05:39 -0700

    Added back all internal utils
    Former-commit-id: 945eecaa9261edb6600520262c778dc3943eacc6

--- R/utils_internal.R
+++ R/utils_internal.R
@@ -1,12 +1,3 @@
-.check_and_load_package <- function(pkg_names) {
-    for (pk in pkg_names) {
-        if (!require(pk, character.only = T)) {
-            err <- sprintf("Package '%s' is not installed.\n", pk)
-            stop(err)
-        }
-    }
-}
-
 
 ## Default ACE values
 .default_rowData <- function(d) {
@@ -30,8 +21,7 @@
 }
 
 .make_chars_unique <- function(x) {
-    x <- as.character(x)
-    make.unique(x, sep = "_")
+    x <- as.character(x) %>% make.unique(., sep = "_")
     return(x)
 }
 
@@ -60,6 +50,14 @@
     }
 }
 
+.check_and_load_package <- function(pkg_names) {
+    for (pk in pkg_names) {
+        if (!require(pk, character.only = T)) {
+            err <- sprintf("Package '%s' is not installed.\n", pk)
+            stop(err)
+        }
+    }
+}
 
 .check_if_ace <- function(sce_like) {
     if (class(sce_like) %in% c("ACTIONetExperiment", "SummarizedExperiment", "SingleCellExperiment")) {
@@ -118,4 +116,68 @@
     } else {
         return(IDX_out)
     }
+}
+
+.preprocess_annotation_features <- function(ace, features_use = NULL) {
+    if (is.null(features_use)) {
+        features_use <- rownames(ace)
+    } else {
+        features_use <- .get_attr_or_split_idx(ace,
+            attr = features_use, return_vec = TRUE,
+            d = 1
+        )
+    }
+    return(features_use)
+}
+
+.preprocess_annotation_labels <- function(labels, ace = NULL) {
+    if (is.null(labels)) {
+        return(NULL)
+    }
+
+    if (is.character(labels)) {
+        if (length(labels) == 1) {
+              labels <- .get_attr_or_split_idx(ace, attr = labels, return_vec = TRUE)
+          } else {
+              labels <- factor(labels)
+          }
+    }
+
+    if ((length(labels) > 1) & is.logical(labels)) {
+        labels <- factor(as.numeric(labels), levels = c(0, 1), labels = c("No", "Yes"))
+    }
+
+    if (is.factor(labels)) {
+        v <- as.numeric(labels)
+        names(v) <- levels(labels)[v]
+        labels <- v
+    }
+    if (is.matrix(labels)) {
+        L <- as.numeric(labels)
+        names(L) <- names(labels)
+        labels <- L
+    }
+
+    if (is.null(names(labels)) | length(unique(names(labels))) > 100) {
+        names(labels) <- as.character(labels)
+    }
+
+    return(labels)
+}
+
+.tscalet <- function(A, center = TRUE, scale = TRUE) {
+    A <- Matrix::t(scale(Matrix::t(A), center = center, scale = scale))
+    return(A)
+}
+
+.preprocess_design_matrix_and_var_names <- function(design_mat, variable_name = NULL) {
+    if (is.null(variable_name)) {
+        variable_name <- colnames(design_mat)[ncol(design_mat)]
+    }
+    vn_idx <- which(variable_name == colnames(design_mat))[1]
+    colnames(design_mat) <- make.names(colnames(design_mat), unique = TRUE, allow_ = FALSE)
+    variable_name <- colnames(design_mat)[vn_idx]
+
+    out <- list(design_mat = design_mat, variable_name = variable_name)
+    return(out)
 }
