bec1ac49f04949492d8730d38ffc3b8c33e5f7fc
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2021-08-08 11:13:26 -0700

    Updated orthoProject function
    Former-commit-id: c16b8b7c3a9c3db0ec832b94f589508cb83ae65a

--- R/utils_public.R
+++ R/utils_public.R
@@ -1,8 +1,12 @@
 #' @export
-orthoProject <- function(A, S) {
-    A = scale(A)
-    S = scale(S)
-    A_r = A - S %*% MASS::ginv(t(S) %*% S) %*% (t(S) %*% A)
-    A_r = scale(A_r)
+orthoProject <- function(A, S, prenorm = F, postnorm = F) {
+    if (prenorm) {
+        A <- scale(A)
+        S <- scale(S)
+    }
+    A_r <- A - S %*% MASS::ginv(t(S) %*% S) %*% (t(S) %*% A)
+    if (postnorm) {
+        A_r <- scale(A_r)
+    }
     return(A_r)
 }
