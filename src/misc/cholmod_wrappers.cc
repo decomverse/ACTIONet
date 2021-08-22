4c65e63f2d51f1b2d5036e99b75e19e726026c11
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2021-08-22 13:50:58 -0700

    Fixed CHOLMOD mess

--- src/misc/cholmod_wrappers.cc
+++ src/misc/cholmod_wrappers.cc
@@ -3,46 +3,50 @@
 
 namespace ACTIONet
 {
-    cholmod_sparse_struct *as_cholmod_sparse(cholmod_sparse_struct *ans,
-                                             sp_mat &A)
+    cholmod_sparse *as_cholmod_sparse(cholmod_sparse *ans,
+                                      sp_mat &A)
     {
         ans->itype = CHOLMOD_INT; /* characteristics of the system */
         ans->dtype = CHOLMOD_DOUBLE;
         ans->packed = true;
 
         A.sync();
-        ans->x = const_cast<void *>(A.values);
-        ans->i = const_cast<void *>(A.row_indices);
-        ans->p = const_cast<void *>(A.col_ptrs);
+        /* DOES NOT WORK!!! arma uses (arma::uword *) whereas CHOLMOD uses (int *)
+        void *x_ptr0 = (void *)const_cast<double *>(reinterpret_cast<const double *>(A.values));
+        void *i_ptr0 = (void *)const_cast<arma::uword *>(reinterpret_cast<const arma::uword *>(A.row_indices));
+        void *p_ptr0 = (void *)const_cast<arma::uword *>(reinterpret_cast<const arma::uword *>(A.col_ptrs));
 
-        /*
-    std::vector<double> x(A.values, A.values + A.n_nonzero);
-    std::vector<int> i(A.row_indices, A.row_indices + A.n_nonzero);
-    std::vector<int> p(A.col_ptrs, A.col_ptrs + A.n_cols + 1);
+        ans->x = x_ptr0;
+        ans->i = i_ptr0;
+        ans->p = p_ptr0;
+    
+        // DOES NOT WORK! Same as above
+        std::vector<double> x(A.values, A.values + A.n_nonzero);
+        std::vector<int> i(A.row_indices, A.row_indices + A.n_nonzero);
+        std::vector<int> p(A.col_ptrs, A.col_ptrs + A.n_cols + 1);
 
-    ans->x = x.data();
-    ans->i = i.data();
-    ans->p = p.data();
-    */
+        ans->x = x.data();
+        ans->i = i.data();
+        ans->p = p.data();
+        */
 
-        /*
-    ans->i = new int[A.n_nonzero];
-    ans->x = new double[A.n_nonzero];
-    double *x_ptr = (double *)ans->x;
-    int *i_ptr = (int *)ans->i;
-    for (int k = 0; k < A.n_nonzero; k++)
-    {
-      x_ptr[k] = A.values[k];
-      i_ptr[k] = A.row_indices[k];
-    }
+        ans->i = new int[A.n_nonzero];
+        ans->x = new double[A.n_nonzero];
+        double *x_ptr = (double *)ans->x;
+        int *i_ptr = (int *)ans->i;
+        for (int k = 0; k < A.n_nonzero; k++)
+        {
+            x_ptr[k] = (A.values)[k];
+            i_ptr[k] = (A.row_indices)[k];
+        }
 
-    ans->p = new int[(A.n_cols + 1)];
-    int *ptr = (int *)ans->p;
-    for (int k = 0; k < A.n_cols + 1; k++)
-    {
-      ptr[k] = A.col_ptrs[k];
-    }
-*/
+        ans->p = new int[(A.n_cols + 1)];
+        int *ptr = (int *)ans->p;
+        for (int k = 0; k < A.n_cols + 1; k++)
+        {
+            ptr[k] = A.col_ptrs[k];
+        }
+
         ans->nrow = A.n_rows;
         ans->ncol = A.n_cols;
         ans->nzmax = A.n_nonzero;
@@ -62,7 +66,7 @@ namespace ACTIONet
                  cholmod_common *chol_cp)
     {
         int t = transpose == 'n' ? 0 : 1; // 'n': computes Ax, 't': computes A'x
-        cholmod_sparse_struct *cha = (cholmod_sparse_struct *)A;
+        cholmod_sparse *cha = (cholmod_sparse *)A;
 
         cholmod_dense chb;
         chb.nrow = transpose == 'n' ? n_rows : n_cols;
@@ -94,13 +98,13 @@ namespace ACTIONet
         cholmod_start(&chol_c);
         chol_c.final_ll = 1;
 
-        cholmod_sparse_struct As;
-        as_cholmod_sparse(&As, A);
+        cholmod_sparse *As = new cholmod_sparse[1];
+        as_cholmod_sparse(As, A);
 
         vec Ax = zeros(A.n_rows);
-        dsdmult('n', A.n_rows, A.n_cols, &As, x.memptr(), Ax.memptr(), &chol_c);
+        dsdmult('n', A.n_rows, A.n_cols, As, x.memptr(), Ax.memptr(), &chol_c);
 
-        //    cholmod_free_sparse(&As, &chol_c);
+        cholmod_free_sparse(&As, &chol_c);
         cholmod_finish(&chol_c);
 
         return (Ax);
@@ -112,18 +116,18 @@ namespace ACTIONet
         cholmod_start(&chol_c);
         chol_c.final_ll = 1;
 
-        cholmod_sparse_struct As;
-        as_cholmod_sparse(&As, A);
+        cholmod_sparse *As = new cholmod_sparse[1];
+        as_cholmod_sparse(As, A);
 
-        cholmod_dense Bd;
-        Bd.nrow = B.n_rows;
-        Bd.ncol = B.n_cols;
-        Bd.d = B.n_rows;
-        Bd.nzmax = B.n_rows * B.n_cols;
-        Bd.xtype = CHOLMOD_REAL;
-        Bd.dtype = 0;
-        Bd.x = (void *)B.memptr();
-        Bd.z = (void *)NULL;
+        cholmod_dense *Bd = new cholmod_dense[1];
+        Bd->nrow = B.n_rows;
+        Bd->ncol = B.n_cols;
+        Bd->d = B.n_rows;
+        Bd->nzmax = B.n_rows * B.n_cols;
+        Bd->xtype = CHOLMOD_REAL;
+        Bd->dtype = 0;
+        Bd->x = (void *)B.memptr();
+        Bd->z = (void *)NULL;
 
         mat res = zeros(A.n_rows, B.n_cols);
         cholmod_dense out;
@@ -137,9 +141,10 @@ namespace ACTIONet
         out.z = (void *)NULL;
 
         double one[] = {1, 0}, zero[] = {0, 0};
-        cholmod_sdmult(As, 0, one, zero, &Bd, &res, &chol_c);
+        cholmod_sdmult(As, 0, one, zero, Bd, &out, &chol_c);
 
-        //    cholmod_free_sparse(&As, &chol_c);
+        cholmod_free_sparse(&As, &chol_c);
+        //cholmod_free_dense(&Bd, &chol_c);
         cholmod_finish(&chol_c);
 
         return (res);
@@ -151,30 +156,49 @@ namespace ACTIONet
         cholmod_start(&chol_c);
         chol_c.final_ll = 1;
 
-        cholmod_sparse_struct As;
-        as_cholmod_sparse(&As, A);
+        cholmod_sparse *As = new cholmod_sparse[1];
+        as_cholmod_sparse(As, A);
 
-        cholmod_sparse_struct Bs;
-        as_cholmod_sparse(&Bs, B);
+        cholmod_sparse *Bs = new cholmod_sparse[1];
+        as_cholmod_sparse(Bs, B);
 
-        cholmod_sparse *ans = cholmod_ssmult(&As, &Bs, CHOLMOD_REAL, true,
-                                             true, chol_c);
-
-        // Repack results as an arma::sp_mat
+        int nrow = A.n_rows, ncol = B.n_cols;
         arma::sp_mat res(A.n_rows, B.n_cols);
 
-        // In order to access the internal arrays of the SpMat class
+        cholmod_sparse *ans = cholmod_ssmult(As, Bs, 0, true,
+                                             true, &chol_c);
+
+        res.mem_resize(static_cast<unsigned>(ans->nzmax));
         res.sync();
 
-        // Making space for the elements
-        res.mem_resize(static_cast<unsigned>(ans->nzmax));
+        double *res_x_ptr = (double *)ans->x;
+        int *res_i_ptr = (int *)ans->i;
+        double *out_x_ptr = (double *)arma::access::rwp(res.values);
+        arma::uword *out_i_ptr = arma::access::rwp(res.row_indices);
+        for (int k = 0; k < ans->nzmax; k++)
+        {
+            out_x_ptr[k] = res_x_ptr[k];
+            out_i_ptr[k] = res_i_ptr[k];
+        }
 
-        // Copying elements
-        std::copy(ans->x, ans->x + ans->nzmax, arma::access::rwp(res.values));
-        std::copy(ans->i, ans->i + ans->nzmax, arma::access::rwp(res.row_indices));
-        std::copy(ans->p, ans->p + B.n_cols + 1, arma::access::rwp(res.col_ptrs));
+        int *res_p_ptr = (int *)ans->p;
+        arma::uword *out_p_ptr = arma::access::rwp(res.col_ptrs);
+        for (int k = 0; k < B.n_cols; k++)
+        {
+            out_p_ptr[k] = res_p_ptr[k];
+        }
+
+        // important: set the sentinel as well
+        arma::access::rwp(res.col_ptrs)[B.n_cols] = ans->nzmax;
+
+        // set the number of non-zero elements
+        arma::access::rw(res.n_nonzero) = ans->nzmax;
+
+        cholmod_free_sparse(&As, &chol_c);
+        cholmod_free_sparse(&Bs, &chol_c);
 
         cholmod_finish(&chol_c);
-        return res;
+
+        return (res);
     }
 }
