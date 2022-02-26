6f751b7cafec2f0370059af23a285ea233a94050
Anna <annashch@insitro.com>
2022-02-26 06:54:15 +0000

    gh-pages branch

--- mini_docker/files/install_actionet.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-Rscript -e "devtools::install_github('shmohammadi86/ACTIONetExperiment', ask = FALSE, upgrade = 'always')"
-Rscript -e "devtools::install_github('shmohammadi86/ACTIONet', ref = 'R-devel', ask = FALSE, upgrade = 'always')"
--- purge_core.sh
+++
@@ -1,4 +0,0 @@
-git submodule deinit -f src/ACTIONet
-rm -rf .git/modules/src/ACTIONet
-git rm -f src/ACTIONet
-rm -rf src/ACTIONet
