51ca905341a039faa0f6d9387900bdce384f7fcf
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2022-05-16 07:40:20 -0700

    Fixed plot.ACTIONet.feature.view

--- R/utils_plotting_ggplot.R
+++ R/utils_plotting_ggplot.R
@@ -78,7 +78,9 @@
       ),
       fill = scales::alpha(c("white"), alpha_val),
       size = text_size,
-      segment.color = "transparent",
+      box.padding = 2,
+      max.overlaps = Inf,
+      min.segment.length = 0,
       force = repel_force
     )
   } else {
