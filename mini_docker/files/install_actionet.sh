005413f225c5341409777088832c6b0f958e11a4
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2022-09-10 17:19:40 -0700

    Changed mini docker and simplified dependencies

--- mini_docker/files/install_actionet.sh
+++ mini_docker/files/install_actionet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-Rscript -e "devtools::install_github('shmohammadi86/ACTIONetExperiment', ask = FALSE, upgrade = 'always')"
-Rscript -e "devtools::install_github('shmohammadi86/ACTIONet', ref = 'R-devel', ask = FALSE, upgrade = 'always')"
+Rscript -e "devtools::install_github('shmohammadi86/ACTIONetExperiment', ask = FALSE, upgrade = 'never')"
+Rscript -e "devtools::install_github('shmohammadi86/ACTIONet', ref = 'R-devel', ask = FALSE, upgrade = 'never')"
