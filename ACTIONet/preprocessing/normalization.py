fixed normalization issue and harmony requirement

ACTIONet/preprocessing/normalization.py
@@ -93,14 +93,13 @@ def normalize(
 ) -> Optional[AnnData]:
     adata = adata.copy() if copy else adata
 
+
     if "norm_method" in adata.uns["metadata"].keys():  # Already normalized? leave it alone!
         # return adata if copy else None
         warnings.warn("AnnData object is prenormalized. Please make sure to use the right assay.")
-
-    adata = adata.copy() if copy else adata
-
-    if layer_key is None and "default_assay" in adata.uns["metadata"].keys():
-        layer_key = adata.uns["metadata"]["default_assay"]
+                 
+    if layer_key is None and "input_assay" in adata.uns["metadata"].keys():
+        layer_key = adata.uns["metadata"]["input_assay"]
 
     if layer_key is not None:
         if layer_key not in adata.layers.keys():
@@ -108,6 +107,18 @@ def normalize(
         S = adata.layers[layer_key]
     else:
         S = adata.X
+
+
+    if sparse.issparse(S):
+        UE = set(S.data)
+    else:
+        UE = set(S.flatten())       
+
+
+    nonint_count = len(UE.difference(set(np.arange(0, max(UE)+1))))        
+    if 0 < nonint_count:
+        warnings.warn("Input [count] assay has non-integer values, which looks like a normalized matrix. Please make sure to use the right assay.")
+   
 
     S = normalize_matrix(
         S,